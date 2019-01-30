e15fcd856e2036c36f8a90bddd7fee548f2f138e
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-01-30 20:28:20 +0530

    prepared to deploy now

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ app.use('/api/posts',posts)
 
 if(process.env.NODE_ENV==='production')
 {
-	app.use(express.static('client/build'))
-
-	app.get("*",(req,res)=>{
-		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-	})
+	app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 }
 
 
