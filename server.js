aae038484273fb5450e22cb66d91021205f043cc
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-01-30 17:34:10 +0530

    prepared to deploy now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const passport=require('passport');
 const bodyparser=require('body-parser');
-const path=require('path');
+// const path=require('path');
 
 const cors=require('cors');
 const users=require('./routes/api/users')
@@ -32,14 +32,14 @@ app.use('/api/users',users)
 app.use('/api/profile',profile)
 app.use('/api/posts',posts)
 
-if(process.env.NODE_ENV==='production')
-{
-	app.use(express.static('./client/build'))
+// if(process.env.NODE_ENV==='production')
+// {
+// 	app.use(express.static('./client/build'))
 
-	app.get("*",(req,res)=>{
-		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-	})
-}
+// 	app.get("*",(req,res)=>{
+// 		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+// 	})
+// }
 
 
 
